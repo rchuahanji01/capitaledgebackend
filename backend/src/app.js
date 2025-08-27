dc8e60aac1c35f47a9603fb3424c371d0692e8ba
unknown <rakesh112233rc@gmail.com>
2025-08-27 05:50:56 -0700

    __origin__

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://capitaledge-2.onrender.com", // frontend URL
+    origin: "https://capitaledgebackend-1.onrender.com", // frontend URL
     credentials: true,
   })
 );
